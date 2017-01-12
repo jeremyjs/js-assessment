530dc88bcfdf4c5fbff7a83c1c1f6fa2e05cc445
Jeremy Meyer <jeremythedev@gmail.com>
2017-01-12 14:32:00 -0800

    feat: flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,7 +1,7 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.flowControlAnswers = {
-  fizzBuzz: function(num) {
+  fizzBuzz: function(inputNumber) {
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
@@ -10,6 +10,17 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    
+    const outputNumber = (typeof inputNumber === 'number') ? inputNumber : false;
 
+    const isFizz = (inputNumber % 3 === 0);
+    const isBuzz = (inputNumber % 5 === 0);
+
+    let outputString = '';
+    if (isFizz) outputString += 'fizz';
+    if (isBuzz) outputString += 'buzz';
+
+    return isFizz || isBuzz ? outputString : outputNumber;
   }
 };
+
